1f996f3c9bba7cd05d57ba38ac7c7e3d5cba2370
Saubhagay <saubhagay.goyal2004@gmail.com>
2026-03-29 16:27:49 +0530

    fix: express 5 wildcard routing for catch-all

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use("/api/v1/tasks", taskRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
   );
 } else {
